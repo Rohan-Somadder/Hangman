670f92b3367a3ac1456a5d99643561095f4575fa
Rohan Somadder <rohansomadder10@gmail.com>
2022-01-03 22:38:49 +0530

    full excecution completed

--- main.py
+++ main.py
@@ -35,16 +35,33 @@ def display_menu():
 
 def game(p):
     random_word = word()
-    w_set = set(random_word)
+    ans = '-'*len(random_word)
     while p.ret_lives():
-        ans = '-'*len(random_word)
-        print(f"Word : {ans}")
-        s = input("Enter : ")
-        if s in w_set:
-            pass
+        print(man(6-p.ret_lives()))
+        print(f"Word : {ans}\t\tLives left : {p.ret_lives()}")
+        ch = input("Enter : ")
+        if ch in random_word:
+            temp_word = ""
+            for i, s in enumerate(random_word):
+                if s == ch:
+                    temp_word += ch
+                elif ans[i] != '-':
+                    temp_word += ans[i]
+                else:
+                    temp_word += '-'
+            ans = temp_word
+            p.update_score(1)
         else:
             print("Your choice isn't present in the word, try again!!!")
             p.update_lives(-1)
+        if ans == random_word:
+            print("Well done, you have won!!!")
+            p.update_score(5)
+            break
+    if ans != random_word:
+        print(
+            f"\nSorry you coudn't guess the word which was : {random_word}\n")
+    p.update_lives(6)
 
 
 def rules():
@@ -55,25 +72,23 @@ def hangman():
     name = input("Enter Your Name: ")
     player = Player(name)
     while True:
-        val = 'N'
         display_menu()
         try:
             inp = int(input("Enter your choice: "))
         except ValueError:
             inp = -1
+        val = 'Y' if 0 < inp <= 3 else 'N'
         if inp == 1:
             game(player)
             val = input("Do you want to play again? (Y/N) : ")
         elif inp == 2:
             print(f"\n{player.ret_name()}'s Score : {player.ret_score()}\n")
-            val = 'Y'
         elif inp == 3:
             rules()
-            val = 'Y'
         if val.upper() != 'Y':
             break
 
 
 if __name__ == '__main__':
     hangman()
-    pause("\n\nThank you for playing!!! \n\nPress any key to exit....")
+    pause("\nThank you for playing!!!\n\nPress any key to exit....")
