c76b7abe13a42d41220f05c73628585d8fce1933
Rohan Somadder <rohansomadder10@gmail.com>
2022-01-03 21:45:48 +0530

    menu Updated

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from words import word
 from body import man
+from getch import pause
 
 
-class player:
+class Player:
     def __init__(self, pname) -> None:
         self.lives = 6
         self.name = pname
+        self.score = 0
 
     def update_lives(self, num):
         self.lives += num
 
+    def update_score(self, num):
+        self.score += num
+
     def ret_lives(self):
         return self.lives
+
+    def ret_name(self):
+        return self.name
+
+    def ret_score(self):
+        return self.score
 
 
 def display_menu():
@@ -22,21 +33,47 @@ def display_menu():
     print("Else -->  Exit")
 
 
+def game(p):
+    random_word = word()
+    w_set = set(random_word)
+    while p.ret_lives():
+        ans = '-'*len(random_word)
+        print(f"Word : {ans}")
+        s = input("Enter : ")
+        if s in w_set:
+            pass
+        else:
+            print("Your choice isn't present in the word, try again!!!")
+            p.update_lives(-1)
+
+
+def rules():
+    print("RULES".center(30, '='))
+
+
 def hangman():
     name = input("Enter Your Name: ")
-    pl = player(name)
-    display_menu()
-    try:
-        inp = int(input("Enter your choice: "))
-    except:
-        inp = -1
-    if inp == 1:
-        pass
-    elif inp == 2:
-        pass
-    elif inp == 3:
-        pass
+    player = Player(name)
+    while True:
+        val = 'N'
+        display_menu()
+        try:
+            inp = int(input("Enter your choice: "))
+        except ValueError:
+            inp = -1
+        if inp == 1:
+            game(player)
+            val = input("Do you want to play again? (Y/N) : ")
+        elif inp == 2:
+            print(f"\n{player.ret_name()}'s Score : {player.ret_score()}\n")
+            val = 'Y'
+        elif inp == 3:
+            rules()
+            val = 'Y'
+        if val.upper() != 'Y':
+            break
 
 
 if __name__ == '__main__':
     hangman()
+    pause("\n\nThank you for playing!!! \n\nPress any key to exit....")
