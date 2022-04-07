aa5ded65a70b05aa82a353f319f420805e5b6e37
Rohan Somadder <rohansomadder10@gmail.com>
2022-04-07 20:49:08 +0530

    Added docstrings to main.py and improved updated play route

--- main.py
+++ main.py
@@ -1,31 +1,76 @@
+'''Main file for the program'''
+from getch import pause
 from words import word
 from body import man
-from getch import pause
 
 
 class Player:
+    '''
+    Class to represent a player
+
+    ...
+
+    Attributes
+    ----------
+    lives : int
+        lives left for the player
+    name : str
+        name of the player
+    score : int
+        score of the player
+
+    Methods
+    -------
+    update_lives(num):
+        Updates the current lives of the player by the given num.
+    update_score(num):
+        Updates the current score of the player by the given num.
+    ret_lives():
+        Returns the current lives of the player for displaying.
+    ret_name():
+        Returns the name of the player.
+    ret_score():
+        Returns the current score of the player for displaying.
+
+    '''
+
     def __init__(self, pname) -> None:
+        """
+        Constructs all the necessary attributes for the Player object.
+        Sets the current score , lives of the player to 0 , 6 respectively.
+
+        Parameters
+        ----------
+            name : str
+                first name of the person
+        """
         self.lives = 6
         self.name = pname
         self.score = 0
 
     def update_lives(self, num):
+        '''Updates the current lives of the player by the given num'''
         self.lives += num
 
     def update_score(self, num):
+        '''Updates the current score of the player by the given num'''
         self.score += num
 
     def ret_lives(self):
+        '''Returns the current lives of the player for displaying'''
         return self.lives
 
     def ret_name(self):
+        '''Returns the name of the player'''
         return self.name
 
     def ret_score(self):
+        '''Returns the current score of the player for displaying'''
         return self.score
 
 
 def display_menu():
+    '''Displays the menu to the player'''
     print(f"\n{'MENU'.center(30,'-')}")
     print("1    -->  Start Game")
     print("2    -->  Score")
@@ -33,45 +78,51 @@ def display_menu():
     print("Else -->  Exit")
 
 
-def game(p):
+def game(game_player):
+    '''Function to handle the logic of the game'''
     random_word = word()
     ans = '-'*len(random_word)
     wrong_choice = ''
-    while p.ret_lives():
-        print(man(6-p.ret_lives()))
-        print(f"Word : {ans}\t\tLives left : {p.ret_lives()}")
+    while game_player.ret_lives():
+        print(man(6-game_player.ret_lives()))
+        print(f"Word : {ans}\t\tLives left : {game_player.ret_lives()}")
         print("Incorrect Guesses : ", wrong_choice)
-        ch = input("Enter : ")
-        if ch in random_word:
+        choice = input("Enter : ")
+        if choice in random_word:
+            # If the entered alphabet is in the word
             temp_word = ""
-            for i, s in enumerate(random_word):
-                if s == ch:
-                    temp_word += ch
-                elif ans[i] != '-':
-                    temp_word += ans[i]
+            for index, alphabet in enumerate(random_word):
+                if alphabet == choice:
+                    temp_word += choice
+                elif ans[index] != '-':
+                    temp_word += ans[index]
                 else:
                     temp_word += '-'
             ans = temp_word
-            p.update_score(1)
+            game_player.update_score(1)
         else:
+            # If the emtered alphabet is not in the word
             print("Your choice isn't present in the word, try again!!!")
-            p.update_lives(-1)
+            game_player.update_lives(-1)
             if not wrong_choice:
-                wrong_choice += ch.upper()
+                wrong_choice += choice.upper()
             else:
-                wrong_choice += " , " + ch.upper()
+                wrong_choice += " , " + choice.upper()
         if ans == random_word:
+            # If the player correctly guessed the word
             print("Well done, you have won!!!")
-            p.update_score(5)
+            game_player.update_score(5)
             break
     if ans != random_word:
+        # If the user exhausts all of the lives
         print(man(6))
         print(
             f"\nSorry you coudn't guess the word which was : {random_word}\n")
-    p.update_lives(6-p.ret_lives())
+    game_player.update_lives(6-game_player.ret_lives())
 
 
 def rules():
+    '''Displays the rules to the player when the player wants to see'''
     print(f"\n{'RULES'.center(30, '=')}")
     print("1. A random word is choosen try to guess it by guessing its letters.")
     print("2. You have 6 lives.")
@@ -81,6 +132,7 @@ def rules():
 
 
 def hangman():
+    '''Main function to control the different functions'''
     print(f"\n{'HANGMAN'.center(30,'~')}\n")
     name = input("Enter Your Name: ")
     player = Player(name)
@@ -94,14 +146,17 @@ def hangman():
         if inp == 1:
             game(player)
             val = input("Do you want to play again? (Y/N) : ")
+            if val.upper() == 'Y':
+                game(player)
         elif inp == 2:
             print(f"\n{player.ret_name()}'s Score : {player.ret_score()}")
         elif inp == 3:
             rules()
-        if val.upper() != 'Y':
+        else:
             break
     pause(
-        f"\nThank you for playing!!! Your total score: {player.ret_score()}\n\nPress any key to exit....")
+        f"\nThank you for playing!!! Your total score: {player.ret_score()}\
+            \n\nPress any key to exit....")
 
 
 if __name__ == '__main__':
